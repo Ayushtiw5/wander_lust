5085313ffc26f7f5059ba3c8cc43dd377fa49966
Ayushtiw5 <iam.ayush886@gmail.com>
2026-04-23 11:57:45 +0530

    fix: resolve multiple listing bugs including image uploads, missing fallbacks, and edit crashes

--- controllers/listings.js
+++ controllers/listings.js
@@ -96,7 +96,7 @@ module.exports.updateListing = async (req, res) => {
     let {id} = req.params;
     let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
 
-    if(typeof req.url !== "undefined"){
+    if(typeof req.file !== "undefined"){
     let url = req.file.path;
     let filename = req.file.filename;
     listing.image = {url, filename};
